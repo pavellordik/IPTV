f5990fd8581e90536c5ee7e788001782b306316b
IPTV <pavelordik@mail.ru>
2026-03-03 23:56:26 +1000

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,20 @@
+import os
+from yadisk import YaDisk
+
+y = YaDisk(token=os.environ['YANDEX_TOKEN'])
+target_dir = '/Приложения/teleplaylists'
+
+if not y.exists(target_dir):
+    y.mkdir(target_dir)
+
+files = [
+    '10_clean.m3u8', '11_clean.m3u8', '2_clean.m3u8', '20TV_clean.m3u8',
+    '3_clean.m3u8', '4_clean.m3u8', '5_clean.m3u8', '6_clean.m3u8',
+    '7_clean.m3u8', '8_clean.m3u8', '9_clean.m3u8', 'fil_mobai_clean.m3u8',
+    'seri_mobai_clean.m3u8', 'staray_seria_clean.m3u8'
+]
+
+for file in files:
+    if os.path.exists(file):
+        y.upload(file, f'{target_dir}/{file}', overwrite=True)
+        print(f'Uploaded {file}')
