c02dbf15998df7e070c77835735e87730da47235
IPTV <pavelordik@mail.ru>
2026-03-04 00:32:54 +1000

    Update upload.py

--- upload.py
+++ upload.py
@@ -4,7 +4,6 @@ from yadisk import YaDisk
 y = YaDisk(token=os.environ['YANDEX_TOKEN'])
 target_dir = '/Приложения/teleplaylists'
 
-# Проверяем, существует ли папка, и создаём, если нет
 if not y.exists(target_dir):
     y.mkdir(target_dir)
 
@@ -12,7 +11,8 @@ files = [
     '10_clean.m3u8', '11_clean.m3u8', '2_clean.m3u8', '20TV_clean.m3u8',
     '3_clean.m3u8', '4_clean.m3u8', '5_clean.m3u8', '6_clean.m3u8',
     '7_clean.m3u8', '8_clean.m3u8', '9_clean.m3u8', 'fil_mobai_clean.m3u8',
-    'seri_mobai_clean.m3u8', 'staray_seria_clean.m3u8'
+    'seri_mobai_clean.m3u8', 'staray_seria_clean.m3u8',
+    'all_clean.m3u8'  # объединённый файл
 ]
 
 for file in files:
